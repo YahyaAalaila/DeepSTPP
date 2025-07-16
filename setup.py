039cf4ddd796bebea21b3df5b892bda2eac977ae
Yahya Aalaila <yahya.aalaila@um6p.ma>
2025-07-16 14:07:11 +0200

    Update: src/deepstpp/

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="deepstpp",
     packages=find_packages(),
     version='0.1.0',
     description='Deep spatiotemporal point process for event forecasting',
